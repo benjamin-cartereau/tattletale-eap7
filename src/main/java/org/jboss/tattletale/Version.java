1db4c96c0fecd391ed8bc74172015d3a15ac00ba
Z016876 <Z016876@ZPPW3106322.r12.an.cnav>
2018-03-26 15:35:29 +0200

    Retrieve version from MANIFEST instead of hard coding it

--- src/main/java/org/jboss/tattletale/Version.java
+++ src/main/java/org/jboss/tattletale/Version.java
@@ -35,7 +35,7 @@ public interface Version
    public static final String PRODUCT = "Tattletale";
 
    /** The version */
-   public static final String VERSION = "1.3 - EAP 7";
+   public static final String VERSION = Version.class.getPackage().getImplementationVersion();;
 
    /** Full version */
    public static final String FULL_VERSION = VENDOR + " " + PRODUCT + " (" + VERSION + ")";
